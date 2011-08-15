2cc4c7dcdde815c5512db2b8fc336f30e77de40d
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-08-15 14:51:00 +0200

    exract the unittest flakes function from the test class

--- pyflakes/test/harness.py
+++ pyflakes/test/harness.py
@@ -6,22 +6,18 @@ import unittest
 
 from pyflakes import checker
 
+def flakes(input, *expectedOutputs, **kw):
+    ast = compile(textwrap.dedent(input), "<test>", "exec",
+                  _ast.PyCF_ONLY_AST)
+    w = checker.Checker(ast, **kw)
+    outputs = [type(o) for o in w.messages]
+    expectedOutputs = list(expectedOutputs)
+    #XXX: use file order or message creation order instead
+    assert sorted(outputs) == sorted(expectedOutputs)
+    return w
 
 class Test(unittest.TestCase):
 
     def flakes(self, input, *expectedOutputs, **kw):
-        ast = compile(textwrap.dedent(input), "<test>", "exec",
-                      _ast.PyCF_ONLY_AST)
-        w = checker.Checker(ast, **kw)
-        outputs = [type(o) for o in w.messages]
-        expectedOutputs = list(expectedOutputs)
-        outputs.sort()
-        expectedOutputs.sort()
-        self.assert_(outputs == expectedOutputs, '''\
-for input:
-%s
-expected outputs:
-%s
-but got:
-%s''' % (input, repr(expectedOutputs), '\n'.join([str(o) for o in w.messages])))
-        return w
+        return flakes(input, *expectedOutputs, **kw)
+
